a1dc9c9d24a1d69990fc10f0d89df50660713c9e
Thulitha <138996396+Deshangaruda@users.noreply.github.com>
2026-04-14 20:54:13 +0530

    Add files via upload

---
+++ Pass.js
@@ -0,0 +1,18 @@
+
+ // Admin login simulation (extra)
+  const loginBtn = document.getElementById('loginBtn');
+  const loginPass = document.getElementById('loginPass');
+  const errorMessage = document.getElementById('error-message');
+
+  if(loginBtn) {
+    loginBtn.addEventListener('click', () => {
+      const password = loginPass.value.trim();
+      if(password === '001@Thuli_$Deshan:T1-M:T') {
+        showToast('✅ Login successful! Redirecting...');
+        window.location.href ="admin1.html";
+      } else {
+        errorMessage.classList.remove('hidden1');
+        loginPass.value = '';
+      }
+    });
+  }
