87a9fa013465464c83f4ff2356fb0ebcc841c295
Thulitha <138996396+Deshangaruda@users.noreply.github.com>
2026-04-16 11:41:01 +0530

    Update Pass.js

--- Pass.js
+++ Pass.js
@@ -9,7 +9,7 @@
       const password = loginPass.value.trim();
       if(password === '001@Thuli_$Deshan:T1-M:T') {
         showToast('✅ Login successful! Redirecting...');
-        window.location.href ="My English Admin.html";
+        window.location.href ="My english Admin.html";
       } else {
         errorMessage.classList.remove('hidden1');
         loginPass.value = '';
